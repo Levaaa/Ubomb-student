59654327ed797315708b3cee42d496204a2de5ff
Levaaa <56501291+Levaaa@users.noreply.github.com>
2020-11-20 17:24:09 +0100

    Update WorldFromFile.java

--- src/main/java/fr/ubx/poo/game/WorldFromFile.java
+++ src/main/java/fr/ubx/poo/game/WorldFromFile.java
@@ -1,19 +1,52 @@
 package fr.ubx.poo.game;
 
 import java.io.BufferedReader;
-import java.io.FILE;
-import java.io.FileWriter;
+import java.io.FileReader;
+import java.io.IOException;
 
 import static fr.ubx.poo.game.WorldEntity.*;
 public class WorldFromFile extends World {
-    private static final WorldEntity[][];
+    private static final WorldEntity[][] mapEntities;
 
-    public WorldFromFile(String name) {
-        String path = "../../ressources/sample/"
+    public WorldFromFile(String name){
+        String path = "src/main/resources/sample/";
         path = path.concat(name);
-        try{
-            File file = new File(path);
-            BufferedReader br = new BufferedReader(new FileWriter(file.getAbsoluteFile()));
+        try{            
+            BufferedReader file = new BufferedReader(new FileReader(path));
+            int data;
+            int x, y = 0;
+
+            while ((data = file.read()) > -1) {
+                char c = (char) data;
+                switch(c){
+                    case '\n' : 
+                        x = 0;
+                        y++;
+                        break;
+                    case '_' :    
+                    case 'B' :
+                    case 'H' :
+                    case 'K' :
+                    case 'M' :
+                    case 'V' :
+                    case 'N' :
+                    case 'n' :
+                    case 'P' :
+                    case 'S' :
+                    case 'T' :
+                    case 'W' :
+                    case '>' :
+                    case '<' :
+                    case '+' :
+                    case '-' :
+                        this.mapEntities[x][y] = ;
+                        break;
+
+                    default :
+                        break;
+                }
+                x++;
+            }   
         } catch (IOException e) {
             e.printStackTrace();
         }
